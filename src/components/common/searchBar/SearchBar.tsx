a686de9323ddebcb3db6d7f2e1c94094fd662466
Simon Oldenhove <simonoldenhove@gmail.com>
2021-03-28 13:44:28 +0200

    search component

---
+++ src/components/common/searchBar/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import classes from './SearchBar.module.css'
+
+interface Props {
+  onChange: () => void;
+}
+
+const SearchBar: React.FC<Props> = (props) => {
+  return <div className=""></div>;
+};
+
+export default SearchBar;
