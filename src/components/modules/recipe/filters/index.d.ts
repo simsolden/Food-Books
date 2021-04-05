8876156bebcfbe72135be996092709408a96ec6e
Simon Oldenhove <simonoldenhove@gmail.com>
2021-04-05 20:45:22 +0200

    filter types created

---
+++ src/components/modules/recipe/filters/index.d.ts
@@ -0,0 +1,18 @@
+export interface Type {
+  aperitif: boolean;
+  entree: boolean;
+  plat: boolean;
+  dessert: boolean;
+}
+
+export interface PrepTime {
+  short: boolean;
+  medium: boolean;
+  long: boolean;
+}
+
+export interface Difficulty {
+  easy: boolean;
+  medium: boolean;
+  hard: boolean;
+}
