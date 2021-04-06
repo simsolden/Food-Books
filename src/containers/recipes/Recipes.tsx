a6473cf4f77a46dfae221c611ec89b92d6df404a
Simon Oldenhove <simonoldenhove@gmail.com>
2021-04-06 22:51:34 +0200

    Some styling

--- src/containers/recipes/Recipes.tsx
+++ src/containers/recipes/Recipes.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import RecipesList from '../../components/modules/recipe/recipesList/RecipesList';
-// import classes from './MyRecipes.module.css'
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import classes from './styles/Recipes.module.css';
 
 interface Props {}
 
 const Recipes: React.FC<Props> = (props) => {
   return (
-    <div className="">
+    <div className={classes.recipes}>
+      <div className={classes.top}>
+        <button className={classes.addRecipe} type="button">
+          Ajouter une recette <LibraryBooksIcon style={{ verticalAlign: 'top' }} fontSize="small" />
+        </button>
+
+        <div>
+          <p>Search bar</p>
+        </div>
+      </div>
       <RecipesList />
     </div>
   );
