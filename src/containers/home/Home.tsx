03f11739535a7a1ce405f950ff87b149a0e3ca45
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-07 10:42:05 +0200

    minor styling on home page

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -9,7 +9,7 @@ interface Props {}
 const Home: React.FC<Props> = (props) => {
   return (
     <div className={classes.home}>
-      <div>
+      <div className={classes.searchBar}>
         <SearchBar
           onSubmit={() => {
             alert('works');
@@ -23,49 +23,69 @@ const Home: React.FC<Props> = (props) => {
         <h2>Vos dernières recettes</h2>
         <div className={classes.lastRecipesCards}>
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
             title="Tacos maison"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/10/26/14/51/peanuts-1771672_960_720.jpg"
             title="Fraise"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2014/05/18/11/49/olive-oil-346997_960_720.jpg"
             title="bouffe"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
             title="Rotî à la sauce du chef..."
           />
         </div>
       </div>
       <div className={classes.lastRecipes}>
-        <h2>Vos classiques</h2>
+        <h2>Découvrez</h2>
         <div className={classes.lastRecipesCards}>
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/10/26/14/51/peanuts-1771672_960_720.jpg"
             title="Fraise"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
             title="Tacos maison"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
             title="Tacos maison"
           />
           <Card
-            style="lastRecipes"
+            type="lastRecipe"
             pictureURI="https://cdn.pixabay.com/photo/2014/05/18/11/49/olive-oil-346997_960_720.jpg"
             title="bouffe"
+          />
+          <Card
+            type="lastRecipe"
+            pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+            title="Tacos maison"
+          />
+          <Card
+            type="lastRecipe"
+            pictureURI="https://cdn.pixabay.com/photo/2016/10/26/14/51/peanuts-1771672_960_720.jpg"
+            title="Fraise"
+          />
+          <Card
+            type="lastRecipe"
+            pictureURI="https://cdn.pixabay.com/photo/2014/05/18/11/49/olive-oil-346997_960_720.jpg"
+            title="bouffe"
+          />
+          <Card
+            type="lastRecipe"
+            pictureURI="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+            title="Rotî à la sauce du chef..."
           />
         </div>
       </div>
