b310cb4b7858fe677874cd7a814c6863a95a66e7
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-07 11:00:30 +0200

    showing recipes according to user and changing css file place

--- src/modules/recipe/components/recipes-list/RecipesList.tsx
+++ src/modules/recipe/components/recipes-list/RecipesList.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
-import classes from './styles/RecipesList.module.css';
+import classes from './RecipesList.module.css';
 
 import Recipe from '../single-recipe/Recipe';
 import Filters from '../filters/Filters';
 
 interface Props {
-  isMine: boolean;
+  userRecipes: boolean;
+  show: boolean;
+  closed: () => void;
 }
 
-const RecipesList: React.FC<Props> = ({ isMine }) => {
+const RecipesList: React.FC<Props> = ({ userRecipes, show, closed }) => {
   return (
     <div className={classes.recipesPage}>
-      <Filters />
+      <Filters show={show} closed={closed} />
       <div className={classes.recipeList}>
         <Recipe
+          isUserRecipe={userRecipes}
           title="Recette numéro uno"
           description="Voici la description uno Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aut ut at repellendus reprehenderit dicta inventore esse fugit? Soluta, asperiores!"
           ingredientsList="Poivrons, Oeufs, Boeuf haché, fromage, poulet, semoule de blé, courgette,... "
         />
         <Recipe
+          isUserRecipe={userRecipes}
           title="Recette numéro Dué"
           description="Voici la description dué"
           ingredientsList="Poivrons, Oeufs, Boeuf haché, fromage, poulet, semoule de blé, courgette,... "
         />
         <Recipe
+          isUserRecipe={userRecipes}
           title="Recette numéro uno"
           description="Voici la description uno Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aut ut at repellendus reprehenderit dicta inventore esse fugit? Soluta, asperiores!"
           ingredientsList="Poivrons, Oeufs, Boeuf haché, fromage, poulet, semoule de blé, courgette,... "
         />
         <Recipe
+          isUserRecipe={userRecipes}
           title="Recette numéro Dué"
           description="Voici la description dué"
           ingredientsList="Poivrons, Oeufs, Boeuf haché, fromage, poulet, semoule de blé, courgette,... "
