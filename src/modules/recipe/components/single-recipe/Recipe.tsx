d742f32a1cf76c9ab2b03c7b5fbc1d2391934d63
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-07 10:59:34 +0200

    recipe with action buttons if "is user's recipe"

--- src/modules/recipe/components/single-recipe/Recipe.tsx
+++ src/modules/recipe/components/single-recipe/Recipe.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import classes from './styles/Recipe.module.css';
+import classes from './Recipe.module.css';
 import StarIcon from '@material-ui/icons/Star';
 import StarHalfIcon from '@material-ui/icons/StarHalf';
+import TodayIcon from '@material-ui/icons/Today';
+import EditIcon from '@material-ui/icons/Edit';
 
 interface Props {
+  isUserRecipe?: boolean;
   title: string;
   description: string;
   ingredientsList: string;
 }
 
-const Recipe: React.FC<Props> = ({ title, description, ingredientsList }) => {
+const Recipe: React.FC<Props> = ({ title, description, ingredientsList, isUserRecipe }) => {
   return (
     <div className={classes.recipe}>
       <a href="#">
         <img src="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg" alt="Tacos" />
       </a>
       <div className={classes.recipeInfo}>
-        <a href="#">
-          <h2>{title}</h2>
-        </a>
+        <div className={classes.firstRow}>
+          <a className={classes.title} href="#">
+            <h2>{title}</h2>
+          </a>
+          {isUserRecipe && (
+            <div className={classes.actionButtons}>
+              <button title="Ajouter au calendrier" onClick={() => alert('add to calendar')}>
+                <TodayIcon fontSize="large" />
+              </button>
+              <button title="Modifier la recette" onClick={() => alert('edit')}>
+                <EditIcon fontSize="large" />
+              </button>
+            </div>
+          )}
+        </div>
         <div className={classes.rating}>
           <p className={classes.stars}>
             <StarIcon fontSize="small" style={{ color: '#CD8282', margin: 0 }} />
@@ -29,8 +44,10 @@ const Recipe: React.FC<Props> = ({ title, description, ingredientsList }) => {
           </p>
           <p className={classes.score}>4,6/5 sur 105 votes</p>
         </div>
-        <p>{description}</p>
-        <p>Ingrédients: {ingredientsList}</p>
+        <div className={classes.extraInfo}>
+          <p>{description}</p>
+          <p>Ingrédients: {ingredientsList}</p>
+        </div>
       </div>
     </div>
   );
