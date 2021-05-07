cd0f7d2ecc3db6fa0e7e79e3b2da233ff565dfe1
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-07 10:48:25 +0200

    recipes pages structure in other component

--- src/containers/my-recipes/MyRecipes.tsx
+++ src/containers/my-recipes/MyRecipes.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import RecipesList from '../../modules/recipe/components/recipes-list/RecipesList';
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import classes from './styles/MyRecipes.module.css';
-import SearchBar from '../../components/searchBar/SearchBar';
+import RecipesPage from '../hoc/RecipesPage';
 
 interface Props {}
 
 const Recipes: React.FC<Props> = (props) => {
-  return (
-    <div className={classes.container}>
-      <div className={classes.recipes}>
-        <div className={classes.top}>
-          <button className={classes.addRecipe} type="button">
-            Ajouter une recette <LibraryBooksIcon style={{ verticalAlign: 'top' }} fontSize="small" />
-          </button>
-          <div>
-            <SearchBar
-              onSubmit={(input) => {
-                alert(`Search: ${input}`);
-              }}
-            />
-          </div>
-        </div>
-        <RecipesList isMine />
-      </div>
-    </div>
-  );
+  return <RecipesPage isUserRecipes/>;
 };
 
 export default Recipes;
--- src/containers/recipes/Recipes.tsx
+++ src/containers/recipes/Recipes.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import RecipesList from '../../modules/recipe/components/recipes-list/RecipesList';
-import classes from './styles/Recipes.module.css';
-import SearchBar from '../../components/searchBar/SearchBar';
+import RecipesPage from '../hoc/RecipesPage';
 
 interface Props {}
 
 const Recipes: React.FC<Props> = (props) => {
-  return (
-    <div className={classes.container}>
-      <div className={classes.recipes}>
-        <div className={classes.top}>
-          <div>
-            <SearchBar
-              onSubmit={(input) => {
-                alert(`Search: ${input}`);
-              }}
-            />
-          </div>
-        </div>
-        <RecipesList isMine={false} />
-      </div>
-    </div>
-  );
+  return <RecipesPage isUserRecipes={false}/>
 };
 
 export default Recipes;
