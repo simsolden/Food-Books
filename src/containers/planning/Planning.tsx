92bcb3a0e77e0b02ce9f892dda6526666e15c067
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-07 11:03:20 +0200

    planning page

--- src/containers/planning/Planning.tsx
+++ src/containers/planning/Planning.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-// import classes from './Planning.module.css'
+import FullPlanning from '../../modules/planning/full-planning/FullPlanning';
+import classes from './Planning.module.css';
 
 interface Props {}
 
 const Planning: React.FC<Props> = (props) => {
   return (
-    <div className="">
-      <h2>Planning</h2>
+    <div className={classes.planningPage}>
+      <FullPlanning />
+      <button title="Générer ma liste de course" className={classes.generateGroceriesButton}>
+        Générer ma liste de course
+      </button>
     </div>
   );
 };
