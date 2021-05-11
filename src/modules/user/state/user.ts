83a0f7948069f6bc8d8b837457e202c6530352c5
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-11 12:24:04 +0200

    user model for redux store

---
+++ src/modules/user/state/user.ts
@@ -0,0 +1,32 @@
+import { User } from '../../../common';
+import { Dispatch } from '../../../store';
+import { UserFactory } from '../factories/UserFactory';
+
+type State = {
+  user: User;
+  isAuthenticated: boolean;
+};
+
+const userInfo = {
+  state: {
+    user: UserFactory.create(),
+    isAuthenticated: true,
+  } as State,
+  reducers: {
+    updateUserInfo: (state: State, user: User) => ({ ...state, user }),
+    setAuthenticated: (state: State, isAuthenticated: boolean) => ({ ...state, isAuthenticated }),
+  },
+  effect: (dispatch: Dispatch) => ({
+    async authenticate(payload: object) {
+      try {
+        const user: User = await new Promise((resolve) => setTimeout(resolve, 1000));
+        dispatch.user.updateUserInfo(user);
+        dispatch.user.setAuthenticated(true);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  }),
+};
+
+export default userInfo;
