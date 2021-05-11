f14c03ea216a5d7a5390ca2de908f62423853b63
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-11 12:21:52 +0200

    AddStepComponent

---
+++ src/modules/recipe/components/add-recipe/add-step/AddStep.tsx
@@ -0,0 +1,41 @@
+import { IconButton } from '@material-ui/core';
+import { error } from 'console';
+import React from 'react';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import classes from './AddStep.module.css';
+import TextInput from '../../../../../components/inputs/text-input/TextInput';
+
+interface Props {
+  index: number;
+  stepDescription: string;
+  onChangeDescription: (value: string) => void;
+  error: boolean;
+  onRemove: () => void;
+}
+
+const AddStep: React.FC<Props> = ({ stepDescription, onChangeDescription, error, onRemove, index }) => {
+  return (
+    <div className={classes.singleStep}>
+      <TextInput
+        label={`Description de l'étape ${index}`}
+        value={stepDescription}
+        onChange={onChangeDescription}
+        error={error}
+        errorMessage="Veuillez entrez une description"
+        type="text"
+        multi
+      />
+      <IconButton
+        aria-label="remove category"
+        component="span"
+        style={{ margin: 'auto', color: '#bc5252' }}
+        onClick={onRemove}
+        title="Supprimer la catégorie"
+      >
+        <HighlightOffIcon />
+      </IconButton>
+    </div>
+  );
+};
+
+export default AddStep;
