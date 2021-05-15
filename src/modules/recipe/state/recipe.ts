53c4174a4e035536e7badd745adcd179ebe00c2b
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-15 18:58:17 +0200

    added mock categories

--- src/modules/recipe/state/recipe.ts
+++ src/modules/recipe/state/recipe.ts
@@ -8,10 +8,31 @@ type State = {
 const recipe = {
   state: {
     categories: [
-      { title: '', description: '' },
-      { title: 'Asiatique', description: '' },
-      { title: 'Africain', description: '' },
-      { title: 'Française', description: '' },
+      {
+        title: 'Asiatique',
+        description: '',
+        pictureUri: 'https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_960_720.jpg',
+      },
+      {
+        title: 'Africaine',
+        description: '',
+        pictureUri: 'https://cdn.pixabay.com/photo/2017/02/19/16/59/tagine-2080272_960_720.jpg',
+      },
+      {
+        title: 'Française',
+        description: '',
+        pictureUri: 'https://cdn.pixabay.com/photo/2016/11/13/05/21/restaurant-1820333_960_720.jpg',
+      },
+      {
+        title: 'Italienne',
+        description: '',
+        pictureUri: 'https://cdn.pixabay.com/photo/2016/09/04/22/07/antipasta-1645401_960_720.jpg',
+      },
+      {
+        title: 'Amérique du nord',
+        description: '',
+        pictureUri: 'https://cdn.pixabay.com/photo/2016/03/26/23/19/hamburger-1281855_960_720.jpg',
+      },
     ],
   } as State,
   reducers: {
