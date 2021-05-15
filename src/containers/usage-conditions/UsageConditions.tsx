d2167a2f494610e0a8ba3cd8587ab227879d9c39
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-15 18:59:40 +0200

    usage conditions, copyright and so on

---
+++ src/containers/usage-conditions/UsageConditions.tsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import classes from './UsageConditions.module.css';
+
+interface Props {}
+
+const UsageConditions: React.FC<Props> = (props) => {
+  return (
+    <div className={classes.container}>
+      <h1 id="conditions-utilisation">Conditions générales d'utilisation</h1>
+      <p>
+        Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du
+        site www.foodbook.fr et de ses services. Par « service », on entend notamment tout contenu accessible via le
+        protocole Internet (site web, application mobile ou tout espace de contribution personnelle).
+        <br />
+        <br />
+        FoodBooks peut faire évoluer librement à tout moment les conditions générales d'utilisation et les conditions
+        spécifiques d'utilisation (propres à certains services, pages, …) afin de les adapter aux évolutions des
+        services et/ou de leur exploitation. Par conséquent, nous vous invitons à vous y référer lors de chaque visite
+        afin de prendre connaissance de leur dernière version disponible en permanence en ligne sur le site FoodBooks.
+        <br />
+        <br />
+        Si une ou plusieurs stipulations des conditions générales d'utilisation sont tenues pour non valides ou
+        déclarées comme telles en application d'une loi, d'un règlement ou d'une décision de justice, les autres
+        stipulations garderont toute leur force et toute leur portée. FoodBooks remplacera la clause déclarée nulle et
+        non valide par une clause qui se rapprochera le plus quant à son contenu de la clause initialement annulée. Les
+        présentes conditions sont régies par le droit belge.
+        <br />
+        <br />
+        Ce contrat est conclu entre : Le gérant du site internet, ci-après désigné « l’Éditeur », Toute personne
+        physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ». Les
+        conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut
+        acceptation de ces conditions.
+      </p>
+      <h2 id="mentions-legales">Mentions légales</h2>
+      <p>
+        Le site foodbooks.fr est édité par Simon Oldenhove, domicilié au 350 chaussée de Boondael, 1050 Bruxelles.
+        <h3>Responsabilité quant au contenu : </h3>
+        • foodbooks.fr dégage toute responsabilité quant à l'origine des recettes. L’Éditeur du site ne pourra en aucun
+        cas être tenu responsable du contenu rentré en ligne. <br />
+        • Si par mégarde, l'une des recettes rentrées est spécifiquement protégée par un copyright et que cette
+        information est portée à notre attention, nous l'enlèverons de notre base, mais dégageons toute responsabilité
+        quant aux droits la concernant. • foodbooks.fr ne peut en aucun cas être considéré comme responsable du résultat
+        d'une des recettes présentes sur le site. Celles-ci sont données à titre indicatif et il est de la
+        responsabilité des utilisateurs de ces recettes de veiller à ce qu'aucun risque ne résulte de la préparation ou
+        de la consommation des produits en résultant.
+        <br />
+        <h3>Propriété intellectuelle</h3>
+        Les contenus du site foodbooks.fr (logos, textes, éléments graphiques, vidéos, etc.) sont protégés par le droit
+        d’auteur, en vertu du Code de la propriété intellectuelle. <br />
+        L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication
+        de ces différents contenus, sauf dans le cadre du site lui-même (par exemple reprendre la recette d’un
+        utilisateur pour la modifier). <br />
+        Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.{' '}
+        <br />
+        L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter
+        atteinte à un tiers. <br />
+        <br />
+        L’Éditeur du site se réserve le droit de modérer ou de supprimer librement et à tout moment les contenus mis en
+        ligne par les utilisateurs, et ce sans justification.
+        <h3 id="copyright">Copyright</h3>
+        Toute reproduction ou copie des contenus présents sur le site (notamment des recettes) à des fins autres qu'un
+        usage privé n'est possible qu'après autorisation écrite des gestionnaires du site. <br />
+        Photos : Flickr, Pixabay, ShutterStock, Pexels, Unsplash <br />
+      </p>
+      <h2 id="services">Services</h2>
+      <p>
+        L’Utilisateur du site FoodBooks a accès aux services suivants :<br />
+        • Voir les recettes des autres utilisateurs si ces derniers ont acceptés de partager les recettes en question.
+        <br />
+        • Voir les différentes catégories/type de cuisine existantes sur le site et les parcourir.
+        <br />
+        • Se créer un compte afin d’accéder aux services mentionnés ci-après.
+        <br />
+        <br />
+        Tout Utilisateur ayant accès à internet peut accéder gratuitement et depuis n’importe où au site. Les frais
+        supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la
+        charge de l’Éditeur.
+        <br />
+        <br />
+        Les services suivants ne sont accessibles pour l’Utilisateur que s’il est membre du site (c’est-à-dire qu’il est
+        identifié à l’aide de ses identifiants de connexion) :<br /> • Créer une recette et la partager ou non.
+        <br />
+        • Enregistrer une recette partagée d’un autre utilisateur et pouvoir ensuite la copier sur le site et/ou la
+        modifier, encore une fois dans le cadre du site foodbooks.fr.
+        <br />
+        • Ajouter ou retirer des recettes dans un planning des 6 prochains jours.
+        <br />
+        <br />
+        Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion
+        d’une maintenance, sans obligation de préavis ou de justification.
+      </p>
+      <h2 id="responsabilite-utilisateur">Responsabilité de l’Utilisateur</h2>
+      <p>
+        L'Utilisateur est responsable des risques liés à l’utilisation de son identifiant de connexion et de son mot de
+        passe.
+        <br />
+        <br />
+        Le mot de passe de l’Utilisateur doit rester secret. En cas de divulgation de mot de passe, l’Éditeur décline
+        toute responsabilité.
+        <br />
+        <br />
+        L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents
+        sur le site foodbooks.fr.
+        <br />
+        <br />
+        Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit
+        faire l'objet d'une indemnisation au profit du site.
+        <br />
+        <br />
+        Le membre s’engage à tenir des propos respectueux des autres et de la loi et accepte que ces publications soient
+        modérées ou refusées par l’Éditeur, sans obligation de justification.
+        <br />
+        <br />
+        En publiant sur le site, l’Utilisateur cède à la société éditrice le droit non exclusif et gratuit de
+        représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers
+        autorisé. L’Éditeur s'engage toutefois à citer le membre en cas d’utilisation de ses recettes, si l’Utilisateur
+        le souhaite.
+        <br />
+        <br />
+        Si vous considérez que votre marque ou votre œuvre a été copiée ou est utilisée d'une manière susceptible de
+        porter atteinte à vos droits tels que reconnus par la législation, merci d'en informer l’éditeur, Simon
+        Oldenhove via l’adresse mail, simonoldenhove@gmail.com.
+        <br />
+        <br />
+        Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité.
+      </p>
+      <h2 id="responsabilite-editeur">Responsabilité de l’Éditeur</h2>
+      <p>
+        Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.
+        <br />
+        <br />
+        De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et
+        insurmontable d'un tiers.
+        <br />
+        <br />
+        Le site foodbooks.fr s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la
+        confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.
+        <br />
+        <br />
+        L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations
+        diffusées sur le site soient réputées fiables.
+        <br />
+        <br />
+        Foodbooks.fr ne garantit pas le résultat des recettes sur son site, ni leur qualité. Foodbooks.fr ne garantit
+        pas que les recettes répondront aux attentes des internautes ou que leurs résultats seront exacts et fiables.
+        <br />
+        En conséquence, la responsabilité de foodbooks.fr ne saurait être engagée en cas d'erreur ou d'omission dans
+        l'une de ces recettes, textes, informations ou illustrations diffusées sur son site.
+      </p>
+      <h2 id="donnees-personnelles">Données personnelles</h2>
+      <p>
+        L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le
+        site. <br />
+        <br />
+        L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site foodbooks.fr pour la
+        communication d’informations diverses et la gestion du compte. <br />
+        <br />
+        L’utilisateur dispose d'un droit d'accès, de rectification, de suppression, d’oubli et d'opposition de ses
+        données personnelles. L'Utilisateur exerce ce droit via l’adresse mail de l’éditeur, simonoldenhove@gmail.com.
+        <br />
+      </p>
+      <h2>Liens hypertextes</h2>
+      <p>
+        Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de
+        l’Éditeur de foodbooks.fr qui n’a pas de contrôle sur ces liens. <br /> Il est possible pour un tiers de créer
+        un lien vers une page du site foodbooks.fr sans autorisation expresse de l’éditeur.
+      </p>
+    </div>
+  );
+};
+
+export default UsageConditions;
