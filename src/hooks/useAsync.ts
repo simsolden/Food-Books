13b785433aa8daab4a70e8e246fa5e8ffd5b8a6d
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-25 11:20:42 +0200

    hookes

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,23 @@
+import { useState, useCallback } from 'react';
+
+export const useAsync = ({ asyncFunction }: { asyncFunction: any }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const execute = useCallback(
+    async (...params) => {
+      try {
+        setLoading(true);
+        const response = await asyncFunction(...params);
+        setResult(response);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    },
+    [asyncFunction]
+  );
+
+  return { error, result, loading, execute };
+};
