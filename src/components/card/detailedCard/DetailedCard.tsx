fa075652d415d8aee04b63bf7b5ad25246ef781f
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-25 21:04:48 +0200

    fetch category onClick

--- src/components/card/detailedCard/DetailedCard.tsx
+++ src/components/card/detailedCard/DetailedCard.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Category } from '../../../common';
 import { IMAGE_SOURCE } from '../../../common/config';
+import { useRematchDispatch } from '../../../hooks/useRematchDispatch';
+import { FiltersFactory } from '../../../modules/recipe/factories/FiltersFactory';
+import { Dispatch } from '../../../store';
 import classes from './DetailedCard.module.css';
 
 interface Props {
@@ -9,12 +12,19 @@ interface Props {
 }
 
 const DetailedCard: React.FC<Props> = ({ category }) => {
+  const setFilters = useRematchDispatch((dispatch: Dispatch) => dispatch.recipe.setFilters);
   const modalClasses = [classes.card];
   modalClasses.push(classes.lastRecipe);
 
+  const handleClick = () => {
+    const filters = FiltersFactory.create();
+    filters.categories = [String(category._id)];
+    setFilters(filters);
+  };
+
   return (
     <div className={modalClasses.join(' ')}>
-      <NavLink to={'decouvrir?page=1&categories=' + category._id}>
+      <NavLink to={'decouvrir?page=1&categories=' + category._id} onClick={handleClick}>
         <h3>{category.title}</h3>
         <img src={IMAGE_SOURCE + category.pictureUri} alt={category.title} />
         <p>{category.description}</p>
