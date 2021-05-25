f807787f36a105f26dc5f41566f39e1f054a441c
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-25 21:04:58 +0200

    sort recipes

--- src/modules/recipe/components/recipes-list/sort-select/SortSelect.tsx
+++ src/modules/recipe/components/recipes-list/sort-select/SortSelect.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useRematchDispatch } from '../../../../../hooks/useRematchDispatch';
+import { Dispatch } from '../../../../../store';
 import classes from './SortSelect.module.css';
 
 interface Props {}
 
 const SortSelect: React.FC<Props> = (props) => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const { setSort } = useRematchDispatch((dispatch: Dispatch) => ({
+    setSort: dispatch.recipe.setSort,
+  }));
+
+  const handleSortChange = (input: ChangeEvent<HTMLSelectElement>) => {
+    let search = new URLSearchParams(location.search);
+    search.set('sort', input.target.value);
+    history.push(`${location.pathname}?${search.toString()}`);
+
+    setSort(input.target.value);
+  };
+
   return (
     <form className={classes.sortSelect}>
       <label>Trier par </label>
-      <select name="sort" id="sort-select">
+      <select name="sort" id="sort-select" onChange={handleSortChange}>
         <option value="">- Sélectionner -</option>
-        <option value="date">Date d'ajout</option>
-        <option value="bestGrade">Meilleure note</option>
-        <option value="popularity">Popularité</option>
-        <option value="price">Prix croissant</option>
-        <option value="time">Temps préparation</option>
+        <option value="-_id">Date d'ajout</option>
+        <option value="-grade">Meilleure note</option>
+        <option value="cost">Prix croissant</option>
+        <option value="prepTime">Temps préparation</option>
       </select>
     </form>
   );
