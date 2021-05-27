302da83c968fc43a9b706b6c582f55aaca50a3f6
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-27 18:32:35 +0200

    info message when there is no planning

--- src/containers/planning/Planning.tsx
+++ src/containers/planning/Planning.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { useRematchDispatch } from '../../hooks/useRematchDispatch';
 import PlanningCard from '../../modules/planning/components/planning-card/PlanningCard';
+import HelpIcon from '@material-ui/icons/Help';
 import { getWeekDay } from '../../modules/planning/utils/getWeekDay';
 import { RootState, Dispatch } from '../../store';
 import classes from './Planning.module.css';
 
 interface Props {}
 
-const Planning: React.FC<Props> = (props) => {
+const Planning: React.FC<Props> = () => {
   const planningRecipes = useSelector((state: RootState) => state.planning.planning);
   const fetchPlanning = useRematchDispatch((dispatch: Dispatch) => dispatch.planning.fetchPlanning);
 
@@ -24,6 +26,15 @@ const Planning: React.FC<Props> = (props) => {
 
   return (
     <div className={classes.planningPage}>
+      {!planningRecipes.length && (
+        <div className={classes.noPlanning}>
+          <HelpIcon className={classes.helpIcon} />
+          <p>
+            Vous pouvez ajoutez des recettes au planning via <Link to="mes-recettes?page=1">Mes recettes</Link> ou la
+            section <Link to="decouvrir?page=1">Découvrir</Link>
+          </p>
+        </div>
+      )}
       <div className={classes.planning}>{planning}</div>;
     </div>
   );
