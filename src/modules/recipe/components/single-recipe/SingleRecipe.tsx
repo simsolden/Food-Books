592bdf3579f308a1f270cc1c5901cceed8069342
Simon Oldenhove <simonoldenhove@gmail.com>
2021-06-21 18:24:27 +0200

    added react print for recipes

--- src/modules/recipe/components/single-recipe/SingleRecipe.tsx
+++ src/modules/recipe/components/single-recipe/SingleRecipe.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useReactToPrint } from 'react-to-print';
+import PrintIcon from '@material-ui/icons/Print';
 import { RecipeRouteParams } from '../../../../common/RouteParams';
 import RecipeMeta from './recipe-meta/RecipeMeta';
 import MainRecipe from './main-recipe/MainRecipe';
 import { useRematchDispatch } from '../../../../hooks/useRematchDispatch';
 import { Dispatch, RootState } from '../../../../store';
-import { useSelector } from 'react-redux';
 import classes from './SingleRecipe.module.css';
 
 interface Props {
@@ -13,6 +15,11 @@ interface Props {
 }
 
 const SingleRecipe: React.FC<Props> = () => {
+  const componentRef = useRef<HTMLDivElement>();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current!,
+  });
+
   const { recipeSlug } = useParams<RecipeRouteParams>();
   const id = recipeSlug.split('_')[1];
 
@@ -29,29 +36,38 @@ const SingleRecipe: React.FC<Props> = () => {
   let [servings, setServings] = useState(recipe.servings);
 
   return (
-    <div className={classes.singleRecipe}>
-      <h1>{recipe.title}</h1>
-      <RecipeMeta
-        recipe={recipe}
-        servings={servings}
-        addServing={() => {
-          setServings((prevServings) => ++prevServings);
-        }}
-        removeServing={() => {
-          setServings((prevServings) => --prevServings);
-        }}
-      />
-      <hr className={classes.hrOne} />
+    <>
+      {/* @ts-ignore */}
+      <div className={classes.singleRecipe} ref={componentRef}>
+        <div className={classes.header}>
+          <h1>{recipe.title}</h1>
+          <button className={classes.printButton} onClick={handlePrint}>
+            <PrintIcon className={classes.printIcon} />
+            <p>Imprimer</p>
+          </button>
+        </div>
+        <RecipeMeta
+          recipe={recipe}
+          servings={servings}
+          addServing={() => {
+            setServings((prevServings) => ++prevServings);
+          }}
+          removeServing={() => {
+            setServings((prevServings) => --prevServings);
+          }}
+        />
+        <hr className={classes.hrOne} />
 
-      <div className={classes.description}>
-        <p>{recipe.description}</p>
+        <div className={classes.description}>
+          <p>{recipe.description}</p>
+        </div>
+        <hr className={classes.hrTwo} />
+        <MainRecipe recipe={recipe} servings={servings} />
+        <p className={classes.recipeCreator}>
+          Merci à <strong>{recipe.username}</strong> pour la délicieuse recette !
+        </p>
       </div>
-      <hr className={classes.hrTwo} />
-      <MainRecipe recipe={recipe} servings={servings} />
-      <p className={classes.recipeCreator}>
-        Merci à <strong>{recipe.username}</strong> pour la délicieuse recette !
-      </p>
-    </div>
+    </>
   );
 };
 
